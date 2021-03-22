9f483f8121fe51aee322eb39345fb4fec28bcc32
YongKim-creator <redgum0319@naver.com>
2021-03-23 08:54:23 +0900

    My Git

---
+++ Git/src/GitTest_04.java
@@ -0,0 +1,10 @@
+
+public class GitTest_04 {
+
+	public static void main(String[] args) {
+		System.out.println("Yesterday was my Birthday");
+		System.out.println("I was so happy");
+
+	}
+
+}
---
+++ Git/src/GitTest_05.java
@@ -0,0 +1,9 @@
+
+public class GitTest_05 {
+
+	public static void main(String[] args) {
+		System.out.println("\"Hello World\"");
+
+	}
+
+}
