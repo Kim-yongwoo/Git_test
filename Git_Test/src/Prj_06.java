20f967a20af93f637f7b74d31bea0fff4207fa6c
YongKim-creator <redgum0319@naver.com>
2021-03-30 08:44:56 +0900

    Project_6

---
+++ Git_Test/src/Prj_06.java
@@ -0,0 +1,16 @@
+
+public class Prj_06 {
+
+	public static void main(String[] args) {
+		int a = 99;
+		
+		if (a<100) { // if문 범위를 잡아줘야함 무조건!
+			System.out.println("100보다 작군요");
+
+	}
+		
+		System.out.println("난 무조건 실행된다.");
+		
+	}
+
+}
