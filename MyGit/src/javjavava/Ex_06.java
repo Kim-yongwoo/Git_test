2a74c5e563ec2155e2bcb58d524d5586b779f844
YongKim-creator <redgum0319@naver.com>
2021-04-03 14:09:18 +0900

    My Git

---
+++ MyGit/src/javjavava/Ex_06.java
@@ -0,0 +1,13 @@
+package javjavava;
+
+import java.util.Scanner;
+
+public class Ex_06 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		float a = sc.nextFloat();
+		System.out.printf("%a", a);
+	}
+
+}
