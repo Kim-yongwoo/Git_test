5f800e6ee429532a920f2d350101b06bee8e7bd5
YongKim-creator <redgum0319@naver.com>
2021-04-04 09:11:57 +0900

    My Git

--- MyGit/src/javjavava/Ex_06.java
+++ MyGit/src/javjavava/Ex_06.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Ex_06 {
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		float a = sc.nextFloat();
 		System.out.printf("%a", a);
