f58fba3db23576243756e93782d14a27a9644bd7
YongKim-creator <redgum0319@naver.com>
2021-04-07 13:47:54 +0900

    Ex_12

---
+++ MyGit/src/javjavava/Ex_12.java
@@ -0,0 +1,16 @@
+package javjavava;
+
+public class Ex_12 {
+
+	public static void main(String[] args) {
+		
+		int hap = 0;
+		int i;
+		
+		for (i = 1; i <= 11; i++) {
+			hap = hap + i;
+		}
+		System.out.printf(" 1에서부터 11까지의 합 : %d", hap);
+	}
+
+}
