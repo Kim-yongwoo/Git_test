96cc51b6a5f09baae2d0e44f381d4f234beb257c
YongKim-creator <redgum0319@naver.com>
2021-04-17 00:33:57 +0900

    Ex_22

---
+++ MyGit/src/javjavava/Ex_22.java
@@ -0,0 +1,25 @@
+package javjavava;
+
+public class Ex_22 {
+
+	public static void main(String[] args) {
+
+		int i = 1;
+		int j = 1;
+		
+		while (i<=9) {
+			j = 1;
+		while (j<=9) {
+			System.out.printf("%d * %d = %d", i, j, i*j);
+			System.out.println();
+			j++;
+		}
+			
+			System.out.println();
+			i++;
+		}
+			
+
+	}
+
+}
